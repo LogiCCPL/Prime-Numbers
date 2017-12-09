f7c21d4011865ad3526a977fd13175ae543e503f
LogiCCPL <34405722+LogiCCPL@users.noreply.github.com>
2017-12-09 23:43:46 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cin >> a;
+	bool tablica[a];
+
+	for(int i = 0; i < a; i++)
+	{
+		tablica[i] = true;
+	}
+	int j = 2;
+	while(j < a)
+	{
+		if(tablica[j]==true)
+		{
+			for(int i = j+j; i < a; i=i+j)
+			{
+			    tablica[i] = false;
+		}
+		}
+		j++;
+	}
+
+	for(int i = 0; i < a; i++)
+	    if(tablica[i]==true)
+	        cout << i << endl;
+}
